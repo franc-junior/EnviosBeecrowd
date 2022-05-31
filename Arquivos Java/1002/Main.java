8e59b5efbebba67b3ef298a141b1a55192cbd8c2
X1K00 <fcju14@gmail.com>
2022-05-30 22:45:49 -0300

    resolução problema do 1031 beecrowd em python

--- Arquivos Java/1002/Main.java
+++ Arquivos Java/1002/Main.java
@@ -1,5 +1,4 @@
 package pkg1002;
-
 import java.util.Scanner;
 
 public class Main {
--- Arquivos Python/Ad-Hoc/1031_Crise de Energia.py
+++ Arquivos Python/Ad-Hoc/1031_Crise de Energia.py
@@ -1,20 +1,23 @@
-n = int(input())
-cont = 18
 while True:
-    lista = list(range(2, n+1))
-    ordem = [1]
-    for i in range(n):
-        ordem.append(lista.pop(cont))
-        lista.extend(lista)
-        cont+=cont-1
-        
-        
-        
+    n = int(input())
+    if n == 0:
+        break
+    num = 0
+    lista=0
+    while lista != [13]:
+        lista = list(range(1,n+1))
+        cont = 0
+        for i in range(n-1):
+            while cont>=len(lista):
+                cont -= len(lista)
+            if lista[cont] == 13:
+                break
+            del lista[cont]
+            cont += num
+        num += 1
+    print(num)
+        # if lista == [13]:
+        #     break
 
 
-        
-        
-        
-
-
-
+# 1,6,11,16,5,12,2,9,17,10,4,15,14,3,8,13,7
