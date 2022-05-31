68393c29299c4656b2bb613fbe61c21f7378a3c5
X1K00 <fcju14@gmail.com>
2022-05-30 22:48:08 -0300

    resolução do problema 1514 beecrowd em python

--- Arquivos Python/Ad-Hoc/1514_Competição.py
+++ Arquivos Python/Ad-Hoc/1514_Competição.py
@@ -1,13 +1,29 @@
-n, m = map(int, input().split())
-cont = 0
-t1 = 0
-t2 = [0]*m
-t3 = 0
-t4 = 0
-for i in range(n):
-    resp = list(map(int,input().split()))
-    if 1 in resp:
-        t4+=1      
-            
-    else:
-        t1+=1
+while True:
+    n, m = map(int, input().split())
+    if n == m == 0:
+        break
+    c1 = 1
+    c2 = [0]*m
+    c3 = 0
+    c4 = 1
+    cont = 0
+    for i in range(n):
+        c = input().split()
+        if "0" not in c:
+            c1 = 0
+        if "1" not in c:
+            c4 = 0 
+        for j in range(m):
+            if c[j] == "1":
+                c2[j] += 1
+    if 0 not in c2:
+        cont+=1
+    if c1 == 1:
+        cont+=1
+    if n not in c2:
+        cont+=1
+    if c4 == 1:
+        cont+=1
+    print(cont)
+
+    
