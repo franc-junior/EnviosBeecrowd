afeadbb5ab4489115cecc33178b831a78f1cf2eb
X1K00 <fcju14@gmail.com>
2022-06-01 03:19:27 -0300

    solução do problema 2651 do beecrowd em java

--- Arquivos Java/Strings/2651-Link Bolado/src/Main.java
+++ Arquivos Java/Strings/2651-Link Bolado/src/Main.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        
+        String s = input.nextLine();
+        String z = "zelda";
+
+        String imprime = "Link Tranquilo";
+
+        int contZ = 0;
+        int i = 0;
+        while(i<s.length()){
+            char letra = Character.toLowerCase(s.charAt(i));
+            char letraZ = z.charAt(contZ);
+            if(letra == letraZ){
+                contZ++;
+                if(contZ == 5){
+                    imprime = "Link Bolado";
+                    break;
+                }
+            }else contZ = 0;
+            i++;
+        }System.out.println(imprime);
     }
 }
