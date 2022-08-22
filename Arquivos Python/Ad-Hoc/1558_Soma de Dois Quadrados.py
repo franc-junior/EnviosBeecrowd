19ff68a668be51611fbf7ccd8a20df3193c5e529
X1K00 <fcju14@gmail.com>
2022-08-22 11:54:40 -0300

    Atualização, não resolvido

--- Arquivos Python/Ad-Hoc/1558_Soma de Dois Quadrados.py
+++ Arquivos Python/Ad-Hoc/1558_Soma de Dois Quadrados.py
@@ -1,3 +1,22 @@
-n = int(input())
-coiso = (n**0.5)
-print(coiso)
+import math
+
+while True:
+    try:
+        n = int(input())
+       
+        quadrado = (n/2)**0.5
+        #print(quadrado)
+
+        x1 = math.floor(quadrado)
+        x2 = math.ceil(quadrado)
+        #print(x1)
+        #print(x2)
+
+        if (x1**2)+(x2**2) == n:
+            print("YES")
+        else:
+            print("NO")
+        
+    except EOFError:
+        break
+    
