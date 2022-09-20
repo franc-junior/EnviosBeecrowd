4fae4dc0e3d44411942314537ad0d8b7c4f78509
X1K00 <fcju14@gmail.com>
2022-09-20 14:30:24 -0300

    Não solução da questão 1258 -> Time limit exceeded

--- Arquivos Python/Estruturas e Bibliotecas/1088_Bolhas e Baldes.py
+++ Arquivos Python/Estruturas e Bibliotecas/1088_Bolhas e Baldes.py
@@ -1,17 +1,16 @@
-while True:
-    nuns = list(map(int, input().split()))
-    if nuns[0] == 0:
-        break
-    n = nuns[0]
+nuns = input().split()
+while nuns[0] != '0':
     p = nuns[1:]
     cont = 0
-    for i in range(n-1):
-        end = p.index(i+1)
-        del p[end]
-        p.insert(i,i+1)
-        cont+=(end-i)
+    for i in range(int(nuns[0])-1):
+        if str(i+1) != p[i]: 
+            end = p.index(str(i+1), i+1)
+            del p[end]
+            p.insert(i,str(i+1))
+            cont+=(end-i)
     if cont%2 == 0:
         print("Carlos")
     else:
         print("Marcelo")
+    nuns = input().split()
 
