262acb2d543a9ccd050fce7ce0c406211e0af7fd
X1K00 <fcju14@gmail.com>
2023-02-06 19:23:29 -0300

    Solução da questão 2386 do beecrowd em python

---
+++ Arquivos Python/Ad-Hoc/2386_Telescópio.py
@@ -0,0 +1,12 @@
+# 40 000 s/m2
+# 10 mm2 == 400 000
+
+# 40 000 000
+# t = 100 mm 
+
+teles = int(input())
+qtd = 0
+for i in range(int(input())):
+    if (teles*int(input()))>=40000000:
+        qtd+=1
+print(qtd)
