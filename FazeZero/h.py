5c94b9f41113d883c5fe367ca0e18dc2949e954a
X1K00 <fcju14@gmail.com>
2024-05-06 12:05:50 -0300

    EXCLUSÃO DE PASTA DESNECESSARIA

--- FazeZero/h.py
+++
@@ -1,8 +0,0 @@
-for i in range(int(input())):
-    cartas = int(input())
-    base = 1
-    while cartas>3:
-        cartas-=3*base+1
-        base+=1
-    print(base-1)
-
--- FazeZero/i.py
+++
@@ -1,8 +0,0 @@
-k = int(input())
-l, r = map(int, input().split())
-string = ""
-cont=0
-while len(string)<=((r+1)-l):
-    string += str(l+cont)
-    cont += 1
-    print(string)
