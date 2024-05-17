662441a1e92f9bc3bb66b04cb7b820ba3915c36f
X1K00 <fcju14@gmail.com>
2024-05-17 13:08:49 -0300

    solução 1018 com js

---
+++ Arquivos JavaScript/Iniciante/1018_Cédulas.js
@@ -0,0 +1,13 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+var valor = parseInt(input);
+
+const notas = [100, 50, 20, 10, 5, 2, 1];
+
+console.log(valor);
+
+for (let nota of notas){
+    let qtdNotas = parseInt(valor / nota);
+    console.log(`${qtdNotas} nota(s) de R$ ${nota},00`);
+    valor %= nota;
+}
